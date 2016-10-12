509ecb738e9b5d06d6510c6a9e693501b561c690
Felix-PC <qzhao8731@gmail.com>
2016-10-11 20:52:22 -0700

    Create project

---
+++ calendarToolKit.js
@@ -0,0 +1,6 @@
+/**
+ * This is a Javascript calendarToolKit plugin. The tool kits now including signle/dual datePiker and multipule date picker with simple notes.
+ *
+ * Author: Felix Zhao
+ * Date: Oct 11, 2016
+ */
